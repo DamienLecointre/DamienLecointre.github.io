9432476753820d053e8e820e507642104294df70
DamienLecointre <damien.lecointre75@gmail.com>
2025-01-03 19:55:38 +0100

    Hero upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,7 @@
-// const textContent = document.querySelector(".text-content");
 const lineBig = document.querySelector(".line-big");
 const lineSmall = document.querySelector(".line-small");
 const emojiContainer = document.querySelector(".emoji-content");
 const emoji = document.querySelector(".emoji");
-// const emojiHands = document.querySelector(".emoji-hands");
 const handBackFist = document.querySelector(".fa-hand-back-fist");
 const handHello = document.querySelector(".fa-hand");
 const handPointRight = document.querySelector(".fa-hand-point-right");
@@ -11,28 +9,28 @@ const nav = document.querySelector("nav");
 
 // console.log(nav);
 
+const setHeroHeight = () => {
+  const vh = window.innerHeight * 1;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+setHeroHeight();
+window.addEventListener("resize", setHeroHeight);
+
 emojiContainer.addEventListener("click", () => {
-  // emojiContainer.classList.add("active");
   lineBig.style.transform = "translateX(100vw)";
-  lineBig.style.transition = "2s ease-in-out";
+  lineBig.style.transition = "2s ease-in";
   lineSmall.style.transform = "translateX(100vw)";
-  lineSmall.style.transition = "2s ease-in-out";
-  emoji.style.transform = "translateX(100vw)";
-  emoji.style.transition = "2s ease-in-out";
-  // emoji.style.animation = "none";
-  // emojiHands.style.transform = "translateX(900%)";
-  // emojiHands.style.transition = "1s ease-in-out";
+  lineSmall.style.transition = "2s ease-in";
+  emoji.style.transform = "translateX(100vw) rotate(30deg)";
+  emoji.style.transition = "2s ease-in";
   // handBackFist.classList.add("show");
   handBackFist.style.transform = "translateX(100vw) rotate(280deg)";
-  handBackFist.style.transition = "2s ease-in-out";
-  // handBackFist.style.animation = "none";
-  // handHello.style.transform = "translateX(100vw)";
-  // handHello.style.transition = "2s ease-in-out";
-  // handHello.style.animation = "none";
+  handBackFist.style.transition = "2s ease-in";
   handPointRight.style.transform = "translateX(100vw)";
   handPointRight.style.opacity = 1;
-  handPointRight.style.transition = "2s ease-in-out";
-  // handPointRight.style.animation = "none";
+  handPointRight.style.visibilty = "visible";
+  handPointRight.style.transition = "2s ease-in";
   nav.style.transform = "translateX(0)";
-  nav.style.transition = "1.2s ease-in-out";
+  nav.style.transition = "1s ease-in";
 });
